b2585579adfcaaaa865be0b9c18493ed1167a373
tresor <100001387+masterdevcode@users.noreply.github.com>
2024-08-20 01:26:49 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 # Exécuter les migrations si la variable d'environnement MIGRATE_ON_START est définie
 if [ "$MIGRATE_ON_START" = "true" ]; then
   echo "Running migrations..."
-  php artisan migrate --force
+  php /var/www/html/artisan migrate --force
 fi
 
 # Créer le lien symbolique pour le stockage
 echo "Creating symlink for storage..."
-php artisan storage:link
+php /var/www/html/artisan storage:link
 
 # Démarrer le serveur PHP-FPM
 exec php-fpm
